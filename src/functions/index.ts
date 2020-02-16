e31bb3e37f1777a79a6bb00307366badfa974e24
Jeroen Claassens <support@favware.tech>
2020-02-16 19:40:38 +0100

    build: fixes for firebase deployment

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,11 +1,12 @@
 import * as functions from 'firebase-functions';
 import next from 'next';
+import { relative } from 'path';
+import { cwd } from 'process';
 
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev, conf: { distDir: 'next' } });
+const app = next({ dev, conf: { distDir: `${relative(cwd(), __dirname)}/../functions/next` } });
 const handle = app.getRequestHandler();
 
 export const favware = functions.https.onRequest((req, res) => {
-  console.log(`File: ${req.originalUrl}`);
   return app.prepare().then(() => handle(req, res));
 });
