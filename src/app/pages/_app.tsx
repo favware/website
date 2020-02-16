51d260cf34dac433be6d45188b19f1c3b9454586
Jeroen Claassens <support@favware.tech>
2020-02-16 19:21:32 +0100

    feat: setup SEO

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -1,9 +1,12 @@
+import { DefaultSeo as DefaultSeoProps } from '@Config/next-seo.config';
 import theme from '@Config/Theme';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
+import { DefaultSeo } from 'next-seo';
 import App from 'next/app';
 import Head from 'next/head';
 import React from 'react';
+import TagManager, { TagManagerArgs } from 'react-gtm-module';
 
 export default class MyApp extends App {
   componentDidMount() {
@@ -12,23 +15,29 @@ export default class MyApp extends App {
     if (jssStyles) {
       jssStyles.parentElement?.removeChild(jssStyles);
     }
+
+    const tagManagerArgs: TagManagerArgs = {
+      gtmId: 'UA-114064877-3'
+    };
+
+    TagManager.initialize(tagManagerArgs);
   }
 
   render() {
     const { Component, pageProps } = this.props;
 
     return (
-      <React.Fragment>
+      <>
         <Head>
-          <title>My page</title>
-          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+          <meta content="ie=edge" httpEquiv="X-UA-Compatible" />
+          <DefaultSeo {...DefaultSeoProps} />
         </Head>
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
           <CssBaseline />
           <Component {...pageProps} />
         </ThemeProvider>
-      </React.Fragment>
+      </>
     );
   }
 }
