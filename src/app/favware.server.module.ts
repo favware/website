cb28782d3b15c326218f1f9305d85ea0a1fe7b8c
Jeroen Claassens <support@favware.tech>
2020-02-16 14:51:00 +0100

    refactor: remove angular-universal interceptor
    ci: run workflow on non-master
    
    ci: add optional [skip ci]

--- src/app/favware.server.module.ts
+++ src/app/favware.server.module.ts
@@ -1,22 +1,13 @@
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FlexLayoutServerModule } from '@angular/flex-layout/server';
 import { ServerModule } from '@angular/platform-server';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
-import { UniversalInterceptor } from '@services/universal-interceptor';
 import { FavwareModule } from './favware.module';
 import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   imports: [FavwareModule, ServerModule, ModuleMapLoaderModule, FlexLayoutServerModule],
   bootstrap: [SidenavComponent],
-  entryComponents: [SidenavComponent],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: UniversalInterceptor,
-      multi: true
-    }
-  ]
+  entryComponents: [SidenavComponent]
 })
 export class FavwareServerModule {}
--- src/util/material.module.ts
+++ src/util/material.module.ts
@@ -2,14 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
@@ -21,7 +18,6 @@ import { MatCarouselModule } from '@ngmodule/material-carousel';
     CommonModule,
     MatButtonModule,
     MatToolbarModule,
-    MatDatepickerModule,
     MatIconModule,
     MatSidenavModule,
     MatListModule,
@@ -31,15 +27,12 @@ import { MatCarouselModule } from '@ngmodule/material-carousel';
     MatSortModule,
     MatInputModule,
     MatCarouselModule,
-    MatProgressSpinnerModule,
-    MatChipsModule,
     MatDividerModule
   ],
   exports: [
     CommonModule,
     MatButtonModule,
     MatToolbarModule,
-    MatDatepickerModule,
     MatIconModule,
     MatSidenavModule,
     MatListModule,
@@ -49,8 +42,6 @@ import { MatCarouselModule } from '@ngmodule/material-carousel';
     MatSortModule,
     MatInputModule,
     MatCarouselModule,
-    MatProgressSpinnerModule,
-    MatChipsModule,
     MatDividerModule
   ]
 })
