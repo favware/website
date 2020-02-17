5f206139bb2f96abcd63266c8396d80461548a92
Jeroen Claassens <support@favware.tech>
2020-02-17 07:17:18 +0100

    style: customize theme

--- src/app/components/Config/Theme.tsx
+++ src/app/components/Config/Theme.tsx
@@ -3,17 +3,28 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
   palette: {
+    common: {
+      white: '#EEEEEE',
+      black: '#23272A'
+    },
+    type: 'dark',
     primary: {
-      main: '#556cd6'
+      main: '#388E3C',
+      light: '#52C7B8',
+      dark: '#00675B',
+      contrastText: '#222222'
     },
     secondary: {
-      main: '#19857b'
+      main: '#074A43',
+      light: '#3C766E',
+      dark: '#00221C',
+      contrastText: '#EEEEEE'
     },
     error: {
       main: red.A400
     },
     background: {
-      default: '#fff'
+      default: '#23272A'
     }
   }
 });
