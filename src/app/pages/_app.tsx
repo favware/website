8044c301212a0529c08327b241adfdcecdc416f8
Jeroen Claassens <support@favware.tech>
2020-02-17 07:17:18 +0100

    style: customize theme

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -2,6 +2,7 @@ import { DefaultSeo as DefaultSeoProps } from '@Config/next-seo.config';
 import theme from '@Config/Theme';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
+import NoScript from '@Pres/NoScript';
 import { DefaultSeo } from 'next-seo';
 import App from 'next/app';
 import Head from 'next/head';
@@ -28,10 +29,20 @@ export default class MyApp extends App {
 
     return (
       <>
+        <DefaultSeo {...DefaultSeoProps} />
         <Head>
           <meta content="ie=edge" httpEquiv="X-UA-Compatible" />
-          <DefaultSeo {...DefaultSeoProps} />
         </Head>
+
+        <NoScript>
+          <span>This site has been build with React and requires that you enable JavaScript to view the full content. Here are some strawberry eating unicorns to motivate you:</span>
+          <p>
+            <span role="img" aria-label="cute unicorn munching on strawberries">
+              🍓🦄🍓🦄🍓🦄
+            </span>
+          </p>
+        </NoScript>
+
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
           <CssBaseline />
--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -1,8 +1,7 @@
-import App from 'components/App';
-import NoScript from 'components/Presentational/NoScript';
-import React from 'react';
-import { NextSeo } from 'next-seo';
 import { createSeoProps } from '@Config/next-seo.config';
+import App from 'components/App';
+import { NextSeo } from 'next-seo';
+import React from 'react';
 
 export default () => (
   <>
@@ -10,14 +9,5 @@ export default () => (
     <App>
       <p>Index Page</p>
     </App>
-
-    <NoScript>
-      <span>This site has been build with React and requires that you enable JavaScript to view the full content. Here are some strawberry eating unicorns to motivate you:</span>
-      <p>
-        <span role="img" aria-label="cute unicorn munching on strawberries">
-          🍓🦄🍓🦄🍓🦄
-        </span>
-      </p>
-    </NoScript>
   </>
 );
