2135e5d3009b67e4ed8237ae9cec77de084b165c
Jeroen Claassens <support@favware.tech>
2020-03-20 23:01:21 +0100

    fix: fixed links in sidebar

--- src/app/components/Material-UI/Link.tsx
+++ src/app/components/Material-UI/Link.tsx
@@ -1,30 +1,34 @@
 import MuiLink from '@material-ui/core/Link';
 import NextComposed from '@Next/NextComposed';
 import clsx from 'clsx';
+import { LinkProps as NextLinkProps } from 'next/link';
 import { useRouter } from 'next/router';
-import React, { forwardRef } from 'react';
+import React, { forwardRef, PropsWithChildren } from 'react';
+import { UrlObject } from 'url';
 
-interface LinkProps {
-  activeClassName: string;
-  as: any;
-  className: string;
-  href: any;
-  innerRef: any;
-  naked: boolean;
-  onClick: any;
-  prefer: boolean;
+interface LinkProps extends NextLinkProps {
+  activeClassName?: string;
+  as?: any;
+  className?: string;
+  href: string | UrlObject;
+  innerRef?: any;
+  naked?: boolean;
+  onClick?: any;
+  prefer?: boolean;
 }
 
-export default forwardRef<any, LinkProps>(({ href, activeClassName = 'active', className: classNameProps, innerRef, naked, ...other }) => {
-  const router = useRouter();
-  const pathname = typeof href === 'string' ? href : href.pathname;
-  const className = clsx(classNameProps, {
-    [activeClassName]: router.pathname === pathname && activeClassName
-  });
+export default forwardRef<any, PropsWithChildren<LinkProps>>(
+  ({ href, activeClassName = 'active', className: classNameProps, innerRef, naked, ...other }) => {
+    const router = useRouter();
+    const pathname = typeof href === 'string' ? href : href.pathname;
+    const className = clsx(classNameProps, {
+      [activeClassName]: router.pathname === pathname && activeClassName
+    });
 
-  if (naked) {
-    return <NextComposed className={className} ref={innerRef} href={href} {...other} />;
+    if (naked) {
+      return <NextComposed className={className} ref={innerRef} href={href} {...other} />;
+    }
+
+    return <MuiLink className={className} ref={innerRef} href={href as string} {...other} />;
   }
-
-  return <MuiLink className={className} ref={innerRef} href={href} {...other} />;
-});
+);
