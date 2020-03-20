b0ef5c946374d9b85c88ff44a86a2f7c10c52f84
Jeroen Claassens <support@favware.tech>
2020-03-21 00:42:59 +0100

    feat: setup more of the home page

---
+++ src/app/components/Material-UI/ProjectCard.tsx
@@ -0,0 +1,88 @@
+import Avatar from '@material-ui/core/Avatar';
+import Box from '@material-ui/core/Box';
+import Card, { CardProps as MUICardProps } from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader, { CardHeaderProps } from '@material-ui/core/CardHeader';
+import IconButton from '@material-ui/core/IconButton';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Link from '@Mui/Link';
+import Tooltip from '@Mui/Tooltip';
+import React, { PropsWithChildren, ReactElement, ReactNode } from 'react';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      maxWidth: 530
+    },
+    media: {
+      height: 0,
+      paddingbottom: '56.25%' // 16:9
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest
+      })
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)'
+    },
+    avatar: {
+      backgroundColor: 'transparent'
+    }
+  })
+);
+
+interface CardAction {
+  to: string;
+  tooltipTitle: string;
+  icon: ReactElement;
+  external?: boolean;
+}
+
+interface CardProps extends MUICardProps {
+  textContent: string;
+  logo: ReactNode;
+  logoAlt: string;
+  cardHeaderProps: CardHeaderProps;
+  actions: CardAction[];
+}
+
+export default ({ textContent, logo, logoAlt, cardHeaderProps, actions, ...props }: PropsWithChildren<CardProps>) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root} {...props}>
+      <CardHeader
+        {...cardHeaderProps}
+        avatar={
+          <Avatar aria-label={logoAlt} alt={logoAlt} className={classes.avatar}>
+            {logo}
+          </Avatar>
+        }
+        titleTypographyProps={{
+          variant: 'h6'
+        }}
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {textContent}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        {actions.map(({ icon, to, tooltipTitle, external = false }, index) => (
+          <Box key={index} component="div">
+            <Link href={to} naked={!external}>
+              <Tooltip title={tooltipTitle} placement="bottom">
+                <IconButton aria-label={tooltipTitle}>{icon}</IconButton>
+              </Tooltip>
+            </Link>
+          </Box>
+        ))}
+      </CardActions>
+    </Card>
+  );
+};
