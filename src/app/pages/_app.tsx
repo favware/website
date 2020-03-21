5468c928bde446d08d2200e1ee7e8d4fc538571f
Jeroen Claassens <support@favware.tech>
2020-03-21 02:49:47 +0100

    feat: add favicon

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -34,6 +34,18 @@ export default ({ Component, pageProps }: PropsWithChildren<AppProps>) => {
       <DefaultSeo {...DefaultSeoProps} />
       <Head>
         <meta content="ie=edge" httpEquiv="X-UA-Compatible" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/manifest.json" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <meta name="apple-mobile-web-app-title" content="Favware" />
+        <meta name="application-name" content="Favware" />
+        <meta name="msapplication-TileColor" content="#00aba9" />
+        <meta name="msapplication-TileImage" content="/favicons/mstile-144x144.png" />
+        <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
 
         <NoScript>
           <span>
