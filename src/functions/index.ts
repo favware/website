4f0eeb87dd9e78e7752501bba6e503b44d218020
Jeroen Claassens <j.claassens@cgi.com>
2020-03-25 11:12:13 +0100

    build: bump dependencies

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,4 +1,4 @@
-import * as functions from 'firebase-functions';
+import { https as firebase } from 'firebase-functions';
 import next from 'next';
 import { relative } from 'path';
 import { cwd } from 'process';
@@ -7,7 +7,7 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev, conf: { distDir: `${relative(cwd(), __dirname)}/../functions/next` } });
 const handle = app.getRequestHandler();
 
-export const favware = functions.https.onRequest(async (req, res) => {
+export const favware = firebase.onRequest(async (req, res) => {
   await app.prepare();
   return handle(req, res);
 });
