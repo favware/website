12b173e54b56cfd4f0236463e73aba767a086590
Jeroen Claassens <support@favware.tech>
2020-03-31 10:08:00 +0200

    build: remove custom webpack plugin

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -1,25 +1,7 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-const { join } = require('path');
-
 module.exports = {
-  webpack: (config) => {
-    if (config.resolve.plugins) {
-      config.resolve.plugins.push(
-        new TsConfigPathsPlugin({
-          configFile: join(__dirname, 'tsconfig.json')
-        })
-      );
-    } else {
-      config.resolve.plugins = [
-        new TsConfigPathsPlugin({
-          configFile: join(__dirname, 'tsconfig.json')
-        })
-      ];
-    }
-
-    return config;
+  experimental: {
+    jsconfigPaths: true,
+    basePath: ''
   },
   distDir: '../../dist/functions/next'
 };
