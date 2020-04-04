a3f7e51ea15094fc491e39ea1942f9ca2ff219ef
Jeroen Claassens <support@favware.tech>
2020-04-04 03:00:58 +0200

    ci: now.sh config

---
+++ .github/next.config.js
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const { join } = require('path');
+
+module.exports = {
+  webpack: (config) => {
+    if (config.resolve.plugins) {
+      config.resolve.plugins.push(
+        new TsConfigPathsPlugin({
+          configFile: join(__dirname, 'tsconfig.json')
+        })
+      );
+    } else {
+      config.resolve.plugins = [
+        new TsConfigPathsPlugin({
+          configFile: join(__dirname, 'tsconfig.json')
+        })
+      ];
+    }
+
+    return config;
+  },
+  target: 'serverless',
+  distDir: '../../.next'
+};
