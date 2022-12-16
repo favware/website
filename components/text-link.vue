f0cdd43adc1c32dcc5549548331d26691978deaf
Jeroen Claassens <support@favware.tech>
2022-12-16 23:37:36 +0100

    fix: update welcome

---
+++ components/text-link.vue
@@ -0,0 +1,7 @@
+<template>
+	<nuxt-link target="_blank" :to="href" class="text-cyan-300 hover:text-cyan-500 visited:text-purple-500">{{ text }}</nuxt-link>
+</template>
+
+<script setup lang="ts">
+defineProps<{ href: string; text: string }>();
+</script>
--- pages/introduction/welcome.vue
+++ pages/introduction/welcome.vue
@@ -4,27 +4,33 @@
 			<div class="lg:flex-grow md:w-full lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
 				<h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 dark:text-gray-100">About me</h1>
 
-				<p class="mb-8 leading-relaxed">Greetings, My name is Jeroen Claassens (a.k.a. Favna) and I manage Favware</p>
+				<p class="mb-8 leading-relaxed">
+					Greetings, My name is Jeroen Claassens (a.k.a. <span title="My online alias!">Favna</span>) and I manage
+					<text-link href="https://github.com/favware" text="Favware" />
+				</p>
 				<p class="mb-8 leading-relaxed">
 					Currently {{ age }} years old I am a developer hailing from The Netherlands. I have been a tech enthusiast for as long as I can
 					remember. While attending high school my first choice of optional subjects was Informatics and this quickly showed to be my
 					calling. After I graduated from high school I had no seconds thoughts about my next step - Software Engineering at a university.
 					The university of choice ended up being The Hague University of Applied Sciences. After 5 years attending this university I
-					obtained my Bachelor of IT in the summer of 2019. At the time of writing I am working for CGI Inc. in The Netherlands as a
-					Frontend Software Engineer.
+					obtained my Bachelor of IT in the summer of 2019. At the time of writing I am working for
+					<text-link href="https://www.cgi.com/nl/nl" text="CGI inc." />
+					in The Netherlands as a Full-Stack Software Engineer. This includes frontend, backend and DevOps work.
 				</p>
 				<p class="mb-8 leading-relaxed">
-					In my free time I have delved into a whole slew of small projects and some of these are used very actively! Notable mentions are
-					my Discord bot "Ribbon" built with NodeJS and a whole slew of NodeJS based libraries. Some of these libraries have their
-					groundwork in JavaScript, however since learning TypeScript I have converted them to this language as these days I have a strong
-					preference for TypeScript and its static type checking. I have also learned C# as I have been delving into the Unity3D Game Engine
-					for the Game Design minor I have followed as part of my Software Engineering major.
+					In my free time I have delved into a whole slew of small to large projects and some of these are used very actively! Notable
+					mentions are my work on the
+					<text-link href="https://sapphirejs.dev" text="Sapphire Discord bot framework and JavaScript utilities" />, the
+					<text-link href="https://github.com/favware/dragonite" text="Dragonite Discord bot" /> and my
+					<text-link href="https://redirect.favware.tech/graphqlpokemon" text="Pokémon API" />. For my other work I implore you to check out
+					<text-link href="https://redirect.favware.tech/github" text="my GitHub profile" />.
 				</p>
 				<p class="mb-8 leading-relaxed">
-					I still have a very broad future ahead of me and I expect that CGI will help me achieve many of my dreams. That said, however,
-					playing games has always been my biggest hobby and now that I have had a taste of creating games I really feel like it would be a
-					perfect match for me. However, I have also grown a passion for the frontend side of software engineering which is why I feel right
-					at home where I found my first job after graduation.
+					Outside of programming I enjoy playing video games, primarily action/adventure. I have a lot of love for Ubisoft's Assassin's
+					Creed series. I also love consuming audiobooks and my favourite author is Brandon Sanderson. I absolutely adore his Cosmere
+					universe. I also enjoy reading the Harry Potter franchise and I tend to re-listen to the series every few years. Overall I have a
+					huge weak spot for most anything fantasy. From time to time I also play Dungeons & Dragons with my friends where I use my love for
+					fantasy to go on epic table top adventures.
 				</p>
 
 				<div class="w-full mb-1 flex flex-wrap justify-center">
