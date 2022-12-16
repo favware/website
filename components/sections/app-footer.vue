354207f39d943ce55fdd5efc6f755900590154ba
Jeroen Claassens <support@favware.tech>
2022-12-17 00:48:34 +0100

    fix: use redirect url for twitter

--- components/sections/app-footer.vue
+++ components/sections/app-footer.vue
@@ -3,7 +3,7 @@
 		<div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
 			<p class="text-sm opacity-70 sm:py-2 sm:mt-0 mt-4">
 				© 2019 Jeroen Claassens —
-				<external-link href="https://twitter.com/@favna_" text="@favna_" />
+				<external-link href="https://redirect.favware.tech/twitter" text="@favna_" />
 			</p>
 		</div>
 	</footer>
