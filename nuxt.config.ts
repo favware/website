c2a4bf33d1ed293115459b904748ce940d346e10
Jeroen Claassens <support@favware.tech>
2022-12-17 01:00:26 +0100

    Revert "feat: add sitemap generator"
    This reverts commit de2c0dde331ba04991c01fe42cd411cd8736c66c.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,9 @@ import 'nuxt';
 const description = "For Hearth and Home! This is Favna's personal website.";
 
 export default defineNuxtConfig({
-	modules: ['@nuxtjs/tailwindcss', '~/modules/sitemap'],
+	modules: ['@nuxtjs/tailwindcss'],
 	typescript: {
 		shim: false
-	},
-	sitemap: {
-		hostname: 'https://favware.tech'
 	},
 	app: {
 		head: {
